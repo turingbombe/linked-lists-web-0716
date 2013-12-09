a6c1c333d397c3fd2bde38ee9d734d1d7158f807
Spencer Rogers <spencer1248@gmail.com>
2013-12-09 10:47:44 -0500

    Add solution for linked list

--- linked-lists/linked_list.rb
+++ linked-lists/linked_list.rb
@@ -1,1 +1,23 @@
-linked_list.rb
+class Deque
+  attr_accessor :queue
+
+  def initialize
+    self.queue = []
+  end
+
+  def push(el)
+    queue.push(el)
+  end
+
+  def pop
+    queue.pop
+  end
+
+  def unshift(el)
+    queue.unshift(el)
+  end
+
+  def shift
+    queue.shift
+  end
+end
